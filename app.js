fa4da2cb14d490663783e716e32c00f2d612bb17
Nadya <nadya.zhidenko@bk.ru>
2021-02-20 18:14:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+$(function(){
+
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+
+$(window).on("scroll load resize",function(){
+	introH = intro.innerHeight();
+	scrollPos = $(this).scrollTop();
+
+	if(scrollPos > introH){
+		header.addClass("fixed");
+	}else{
+		header.removeClass("fixed");
+	};
+});
+
+$("[data-scroll]").on("click", function(event){
+	event.preventDefault();
+
+	let elementId = $(this).data("scroll");
+	let elementOffset = $(elementId).offset().top;
+
+	 $("html, body").animate({
+        scrollTop:elementOffset - 70 
+    }, 1000);
+
+
+});
+
+// NavToggle
+let nav = $("#nav");
+let navToggle = $("#navToggle");
+
+navToggle.on("click", function(event){
+	event.preventDefault();
+
+	nav.toggleClass("show");
+});
+
+});
